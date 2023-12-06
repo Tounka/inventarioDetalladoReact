3708381b711fb51744efc0bd342c6a9916c362e0
Tounka <luisydiego201516@gmail.com>
2023-12-06 15:34:30 -0700

    version estable

--- src/componentes/InputID.jsx
+++ src/componentes/InputID.jsx
@@ -6,12 +6,12 @@ function InputID({ setNombre, setCdp }){
     return(
         <div className="contenedorInputID">
             <div className="contenedorInputNombre">
-                <label htmlFor="exampleInputEmail1" className="form-label">Ingresa tu Nombre</label>
+                <label htmlFor="exampleInputEmail1" className="form-label txtLabelInput">Ingresa tu Nombre</label>
                 <input type="text" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Ramon" onChange={(e) => setNombre(e.target.value)}/>
                 
             </div>
             <div className="contenedorInputCdp">
-                <label htmlFor="exampleInputPassword1" className="form-label">Ingresa tu CDP</label>
+                <label htmlFor="exampleInputPassword1" className="form-label txtLabelInput">Ingresa tu CDP</label>
                 <input type="text" className="form-control" id="exampleInputPassword1" placeholder="Colosio"  onChange={(e) => setCdp(e.target.value)}/>
             </div>
         </div>
