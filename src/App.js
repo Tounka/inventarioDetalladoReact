98916a02e2726429baed40d20aed6db7da16ba31
Tounka <luisydiego201516@gmail.com>
2024-01-12 08:50:55 -0700

    agregada libreria styledComponents y pagina de audotoria

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 
 import PaginaInventarioDetallado from './Paginas/PaginaInventarioDetallado'
 import PaginaMcHoot from './Paginas/PaginaMcHoot';
+import PaginaMcAuditoria from './Paginas/PaginaMcAuditoria';
 import NotFound from './Paginas/NotFound'
 
 import { BrowserRouter , Route, Routes} from 'react-router-dom';
@@ -15,6 +16,7 @@ function App() {
       <Routes>
         <Route path='InventarioDetallado' element={<PaginaInventarioDetallado />} />
         <Route path='McHoot' element={<PaginaMcHoot />} />
+        <Route path='McAuditoria' element={<PaginaMcAuditoria />} />
         <Route path='*' element={<NotFound />} />
       </Routes>
     </BrowserRouter>
---
+++ src/Paginas/PaginaMcAuditoria.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import '../hojas-de-estilo/base.css'
+function PaginaMcAuditoria(){
+
+    return(
+        <div className="container-sm bggeen">
+            <h1>McAuditoria</h1>
+        </div>
+        
+    );
+}
+
+export default PaginaMcAuditoria;
