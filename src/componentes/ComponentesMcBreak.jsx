0e249d2ba1b35df29bbeebfcdc9fd424d6d31bc8
Tounka <luisydiego201516@gmail.com>
2024-03-11 21:47:58 -0700

    agregada funcion de suma de tiempo

--- src/componentes/ComponentesMcBreak.jsx
+++ src/componentes/ComponentesMcBreak.jsx
@@ -2,8 +2,8 @@ import React from "react";
 import styled from "styled-components";
 
 const ContenedorCardMcBreak = styled.div`
-    height: 250px;
-    width: 150px;
+    height: 300px;
+    width: 175px;
     background-color: #B41A4A;
     border-radius: 30px;
     overflow: hidden;
@@ -15,11 +15,39 @@ const ImagenCardMcBreak = styled.img`
     object-fit: cover;
     
 `
+const P = styled.p`
+    font-size: 30px;
+    font-weight:bold;
+    text-align: center;
+    margin: 5px;
+`
+const Psecundario = styled(P)`
+    font-size: 15px;
+    
+`
 export function CardMcBreak({SrcImg, Nombre}){
+    const handleClick = () => {
+        const tiempo = new Date();
+        const tiempoDeRegreso = new Date(tiempo);
+        tiempoDeRegreso.setMinutes(tiempo.getMinutes() + 30);
+        const opcionesFormato = {
+            hour: 'numeric',
+            minute: 'numeric',
+            second: 'numeric',
+            hour12: true, 
+          };
+        
+        console.log(tiempo.toLocaleTimeString('en-US', opcionesFormato));
+        console.log(tiempoDeRegreso.toLocaleTimeString('en-US', opcionesFormato));
+        
+    }
     return(
-        <ContenedorCardMcBreak>
+        <ContenedorCardMcBreak onClick={handleClick}>
             <ImagenCardMcBreak src={SrcImg}   />
-            <h1>{Nombre}</h1>
+            <P>{Nombre}</P>
+            <Psecundario>{Nombre}</Psecundario>
+            
+
         </ContenedorCardMcBreak>
     );
 } 
