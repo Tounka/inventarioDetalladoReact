93817aa271c37faa7596ac98ca248e31c21ed5d0
Tounka <luisydiego201516@gmail.com>
2024-03-13 11:14:29 -0700

    v2

--- src/componentes/ComponentesMcBreak.jsx
+++ src/componentes/ComponentesMcBreak.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import styled from "styled-components";
+import { useState } from "react";
 
 const ContenedorCardMcBreak = styled.div`
     height: 300px;
@@ -25,27 +26,63 @@ const Psecundario = styled(P)`
     font-size: 15px;
     
 `
+const ContenedorPsecundarios = styled.div`
+    display: flex;
+    width: 100%;
+    justify-content:center;
+`
 export function CardMcBreak({SrcImg, Nombre}){
-    const handleClick = () => {
-        const tiempo = new Date();
-        const tiempoDeRegreso = new Date(tiempo);
-        tiempoDeRegreso.setMinutes(tiempo.getMinutes() + 30);
+    const [Tiempo1,  setTiempo1] = useState(0);
+    const [Tiempo2,  setTiempo2] = useState(0);
+    const [Text,  setText] = useState('');
+    const [switchFuncion,  setSwitchFuncion] = useState(false);
+
+    const handleClick = (switchFuncion) => {
         const opcionesFormato = {
             hour: 'numeric',
             minute: 'numeric',
-            second: 'numeric',
-            hour12: true, 
+            
+            hour12: false, 
           };
-        
-        console.log(tiempo.toLocaleTimeString('en-US', opcionesFormato));
-        console.log(tiempoDeRegreso.toLocaleTimeString('en-US', opcionesFormato));
+        if(switchFuncion){
+            let tiempoActual = new Date();
+            let tiempoDeRegreso = Tiempo2;
+            tiempoActual= tiempoActual.toLocaleTimeString('en-US', opcionesFormato);
+            
+            setText(tiempoDeRegreso + '/' + tiempoActual);
+
+        }else{
+            const tiempo = new Date();
+            const tiempoDeRegreso = new Date(tiempo);
+            tiempoDeRegreso.setMinutes(tiempo.getMinutes() + 30);
+       
+            
+              setTiempo1(tiempo.toLocaleTimeString('en-US', opcionesFormato));
+              setTiempo2(tiempoDeRegreso.toLocaleTimeString('en-US', opcionesFormato));
+        }
+       setSwitchFuncion(!switchFuncion);
+
         
     }
     return(
-        <ContenedorCardMcBreak onClick={handleClick}>
+        <ContenedorCardMcBreak onClick={() => handleClick(switchFuncion)}>
+
             <ImagenCardMcBreak src={SrcImg}   />
             <P>{Nombre}</P>
-            <Psecundario>{Nombre}</Psecundario>
+            {Tiempo1 !== 0 ? (
+                     !switchFuncion  ? (
+                        <ContenedorPsecundarios>
+                            <Psecundario>{Tiempo1}</Psecundario>
+                            <Psecundario>-</Psecundario>
+                            <Psecundario>{Tiempo2}</Psecundario>
+                        </ContenedorPsecundarios>
+                    ) : (
+                        <Psecundario>
+                            {Text}
+                        </Psecundario>
+                    )
+            ) : null}
+            
             
 
         </ContenedorCardMcBreak>
