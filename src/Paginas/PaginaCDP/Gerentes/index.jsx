1f0affe25d1570b5a0a10512cc35aca12248babd
Tounka <luisydiego201516@gmail.com>
2024-09-26 16:29:56 -0700

    Mejoras

--- src/Paginas/PaginaCDP/Gerentes/index.jsx
+++ src/Paginas/PaginaCDP/Gerentes/index.jsx
@@ -2,15 +2,21 @@
 import { PaginaCDPCrewUx, PaginaCDPGerenteUx } from "./PaginaCDPGerentesUx"
 import TituloPrincipal from "../../../componentes/TituloPagina"
 import Footer from "../../../componentes/Footer"
+import { DisplayGenerico } from "../../../componentes/Displays"
+import styled from "styled-components"
 
+import { useNavigate } from "react-router-dom"
+import { useCdp } from "../../Contextos/ContextoCDP"
 
 export const VisualizadorCDPs = () =>{
-   
+   const {tareasCDP} = useCdp();
    
     return(
         <>
             <TituloPrincipal textoTitulo="CDPs"  />
                 <PaginaCDPGerenteUx />
+
+                
             <Footer textoPrincipalFooter='No Se Trata De Un Software Desarrollado Por ARCOS DORADOS.'  textoSecundarioFooter='Desarrollado por Ramon Castillo'  /> 
         </>
         
